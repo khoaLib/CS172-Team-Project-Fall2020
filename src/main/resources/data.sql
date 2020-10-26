1bb1aa37c0989c7adb960bc66b2eead46e42db8f
khoaLib <phamkhoa996@gmail.com>
2020-10-25 22:34:02 -0700

    springboot + react

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO beer(name,abv) VALUES('Jai Alai', 7.5);
+INSERT INTO beer(name,abv) VALUES('Stella Artois', 5.0);
+INSERT INTO beer(name,abv) VALUES('Lagunitas IPA', 6.2);
+INSERT INTO beer(name,abv) VALUES('Lab 7 is worked', 10.0);
+COMMIT;
